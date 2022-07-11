fb4427359d26198f3ecff55e93f950930ad84934
Zdenek Grof <zdenek.grof@vscht.cz>
2022-07-11 20:33:20 +0200

    Optimize Day15 to run faster

--- src/day15.f90
+++ src/day15.f90
@@ -1,3 +1,6 @@
+!
+! DAY 15 - 
+!
 module day15_mod
   use tree_m, only : rbtr_t, tree_mold, DAT_KIND
   use abstract_container, only : ERR_CONT_OK, ERR_CONT_ISNOT, ERR_CONT_IS
@@ -7,7 +10,7 @@ module day15_mod
 
   type hash_t
     integer :: key
-    integer :: val
+    integer, pointer :: val ! to update its value directly
   end type
 
   type game_t
@@ -17,9 +20,26 @@ module day15_mod
   contains
     procedure :: init => game_init
     procedure :: one_round => game_next
+    final :: game_finalize
   end type game_t
 
 contains
+
+  subroutine game_finalize(this)
+    type(game_t), intent(inout) :: this
+    type(hash_t) :: dat
+    integer(DAT_KIND), allocatable :: handle(:)
+    integer :: ierr
+    call this%hash % Resetcurrent(handle)
+    do
+      dat = transfer(this%hash % NextRead(handle,ierr), dat)
+      if (ierr /= 0) exit
+      deallocate(dat%val)
+    end do
+    call this%hash % Removeall()
+  end subroutine
+
+
 
   subroutine game_init(this, starting_arr)
     class(game_t), intent(out) :: this
@@ -32,6 +52,7 @@ contains
     this%prev_said = starting_arr(n0)
     do i=1, n0-1
       call hash_update(this%hash, starting_arr(i), i)
+      ! WARNING - elements in starting_arr must be unique
     end do
   end subroutine game_init
 
@@ -40,12 +61,14 @@ contains
   subroutine game_next(this)
     class(game_t), intent(inout) :: this
     integer :: val
+    integer(DAT_KIND), allocatable :: handle(:)
 
-    val = hash_get(this%hash, this%prev_said)
-    call hash_update(this%hash, this%prev_said, this%time)
+    val = hash_get(this%hash, this%prev_said, handle)
     if (val == 0) then 
+      call hash_update(this%hash, this%prev_said, this%time)
       this%prev_said = 0
     else
+      call hash_update(this%hash, this%prev_said, this%time, handle)
       this%prev_said = this%time - val
     end if
     this%time = this%time + 1
@@ -54,34 +77,51 @@ contains
 
 
 
-  subroutine hash_update(hash, key, val)
+  subroutine hash_update(hash, key, val, handle)
     type(rbtr_t), intent(inout) :: hash
     integer, intent(in) :: key, val
+    integer(DAT_KIND), allocatable, intent(in), optional :: handle(:)
 !
-! Update pair "key - val" in hash, or store new one
+! Update pair "key - val" in hash, or store new one.
+! We made search before: "handle" present -> update the value
+! "handle" not present -> add new node
 !
     type(hash_t) :: olddat, newdat
     integer :: ierr
 
     olddat % key = key
     newdat % key = key
-    newdat % val = val
-    if (hash % Isin(transfer(olddat,tree_mold))) then
-      call hash % Update(transfer(olddat,tree_mold), transfer(newdat,tree_mold))
+
+    if (present(handle)) then
+      ierr = ERR_CONT_OK
     else
-      call hash % Add(transfer(newdat,tree_mold))
+      !call hash % Find(transfer(olddat,tree_mold), handle, ierr)
+      ierr = ERR_CONT_ISNOT
     end if
+
+    select case(ierr)
+    case(ERR_CONT_OK)
+      !call hash % UpdateCurrent(handle, transfer(newdat,tree_mold))
+      olddat = transfer(hash % Read(handle), olddat)
+      olddat % val = val
+    case(ERR_CONT_ISNOT)
+      allocate(newdat%val)
+      newdat % val = val
+      call hash % Add(transfer(newdat,tree_mold))
+    case default
+      error stop 'hash_update - uknown exit code'
+    end select
   end subroutine hash_update
 
 
 
-  integer function hash_get(hash, key) result(val)
+  integer function hash_get(hash, key, handle) result(val)
     type(rbtr_t), intent(in) :: hash
     integer, intent(in) :: key
+    integer(DAT_KIND), allocatable, intent(out) :: handle(:)
 !
 ! Ask, if "key" has been stored. Return "val" or "0" (no entry)
 !
-    integer(DAT_KIND), allocatable :: handle(:)
     integer :: ierr
     type(hash_t) :: dat
 
